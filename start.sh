383e70fb509750c8c99160580607f22707fd9559
Ryan Bennett <your@mail.com>
2023-05-11 04:46:55 -0600

    add node-red and enable projects

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 source .env.local
-docker run --rm -e "NODE_RED_CREDENTIAL_SECRET=$NODE_RED_CREDENTIAL_SECRE" -p 1880:1880 -v `pwd`:/data --name red-gpt red-gpt
+docker run --rm -e NODE_RED_ENABLE_PROJECTS=true -e "NODE_RED_CREDENTIAL_SECRET=$NODE_RED_CREDENTIAL_SECRE" -p 1880:1880 -v `pwd`:/data --name red-gpt red-gpt
